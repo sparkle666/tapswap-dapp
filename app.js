d271ccb7b65c5407b50acb9e4a17999fe5217470
Sparkle666 <sikky606@gmail.com>
2024-12-09 23:40:37 +0100

    added lesson on import and export

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
-import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
-import { greet } from "./config.js";
+// This is known as named export of functions
 
+export const owner = "James"
 
-console.log(ethers)
+export function addTwoNumbers(num1, num2){  
+    console.log((num1 + num2) ** 12)
 
-const provider = new ethers.BrowserProvider(window.ethereum)
-
-console.log("provider: ", provider)
-
-
-
-export const fetchBlockchainData = async () => {
-    const network = await provider.getNetwork();
-    console.log("Connected to:", network.name, "Chain ID:", network.chainId);
-
+    return (num1 + num2) ** 12
 }
 
-greet()
+export function addThreeNumbers(num1, num2, num3){
+    return num1 + num2 + num3
+}
 
+// This is known as default export of functions
+function addFourNumbers(num1, num2, num3){
+    // console.log(num1 + num2 + num3)
+    return num1 + num2 + num3
+}
+export default addFourNumbers
 
-class Person {
-    constructor(name, age) {
-      this.name = name;
-      this.age = age;
-    }
-  
-    greet() {
-      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
-  }
-  
-  const person1 = new Person('Alice', 30);
-  person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
+let sum = addTwoNumbers(20, 80)
 
+console.log(sum)
---
+++ utils.js
@@ -0,0 +1,16 @@
+import { addTwoNumbers } from "./app.js"
+import { addThreeNumbers } from "./app.js"
+import { owner } from "./app.js"
+
+import addFourNumbers from "./app.js"
+
+
+console.log(owner)
+
+addFourNumbers(90, 90, 90)
+
+// console.log(sum)
+
+
+
+// 
