ce061408c9626af9cadd709352f61c7673f744d5
Sparkle666 <sikky606@gmail.com>
2024-12-19 00:26:14 +0100

    added dapp

---
+++ dapp.js
@@ -0,0 +1,22 @@
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+import { Wallet } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+
+// Check if user has the metamask extension exists
+
+// let hasMetamask
+
+// if(window.ethereum.isMetaMask){
+//     hasMetamask = true;
+
+//     console.log("You have metamask installed")
+// } else{
+//     console.log("You don't have metamask installed on your browser.")
+// }
+
+
+// // See how to create a wallet address in ethersjs, how to create a private key, and Mnemonics
+
+
+// const wallet = Wallet.createRandom()
+
+// console.log(`Your wallet address: ${wallet.address} : Your Mnemonics: ${wallet.mnemonics} Your private key:`)
